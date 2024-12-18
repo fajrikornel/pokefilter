eb946cebee47545e5a5b31ffa6ba074f22ce6324
Fajri Kornel <fajri.apk@gmail.com>
2024-12-18 11:18:40 +0700

    feat: add TypeFilter implementation

--- internal/filter/type_filter.go
+++ internal/filter/type_filter.go
@@ -1,1 +1,103 @@
 package filter
+
+import (
+	"errors"
+	"fmt"
+	"strings"
+)
+
+type TypeFilter struct {
+	type1        string
+	type2        string
+	excludeTypes []string
+	chain        Filter
+}
+
+func NewTypeFilter(type1, type2 string, excludeTypes []string) (*TypeFilter, error) {
+	if type1 == "" && type2 == "" {
+		return nil, errors.New("type1 and type2 cannot be empty")
+	}
+
+	if type2 != "" && len(excludeTypes) > 0 {
+		return nil, errors.New("cannot specify both types and specify excludeTypes too")
+	}
+
+	for _, excludeType := range excludeTypes {
+		if type1 == excludeType {
+			return nil, errors.New("excluded type is filtered")
+		}
+	}
+
+	return &TypeFilter{
+		type1:        type1,
+		type2:        type2,
+		excludeTypes: excludeTypes,
+	}, nil
+}
+
+func (f *TypeFilter) BuildQuery() (string, error) {
+	var query string
+	if f.type2 != "" {
+		query = fmt.Sprintf(`
+			SELECT 
+				p.id as pokemon_id 
+			FROM 
+				pokemon p 
+				LEFT JOIN types t1 on p.type_1_id = t1.id 
+				LEFT JOIN types t2 on p.type_2_id = t2.id 
+			WHERE 
+				(t1.name = '%s' AND t2.name = '%s') OR 
+				(t2.name = '%s' AND t1.name = '%s') 
+			`, f.type1, f.type2, f.type1, f.type2)
+	} else {
+		if f.excludeTypes != nil && len(f.excludeTypes) > 0 {
+			excludeTypesString := "'" + strings.Join(f.excludeTypes, "', '") + "'"
+			query = fmt.Sprintf(`
+				SELECT 
+					p.id as pokemon_id 
+				FROM 
+					pokemon p 
+					LEFT JOIN types t1 on p.type_1_id = t1.id 
+					LEFT JOIN types t2 on p.type_2_id = t2.id 
+				WHERE 
+					(t1.name = '%s' AND t2.name NOT IN (%s)) OR 
+					(t2.name = '%s' AND t1.name NOT IN (%s)) 
+				`, f.type1, excludeTypesString, f.type1, excludeTypesString)
+		} else {
+			query = fmt.Sprintf(`
+				SELECT 
+					p.id as pokemon_id 
+				FROM 
+					pokemon p 
+					LEFT JOIN types t1 on p.type_1_id = t1.id 
+					LEFT JOIN types t2 on p.type_2_id = t2.id 
+				WHERE 
+					t1.name = '%s' OR t2.name = '%s' 
+				`, f.type1, f.type1)
+		}
+	}
+
+	var chainQuery string
+	var err error
+	if f.chain != nil {
+		chainQuery, err = f.chain.BuildQuery()
+	} else {
+		chainQuery, err = NewTerminalFilter().BuildQuery()
+	}
+
+	if err != nil {
+		return "", err
+	}
+
+	query += fmt.Sprintf(" AND p.id IN (%s)", chainQuery)
+	return query, nil
+}
+
+func (f *TypeFilter) Chain(other Filter) (Filter, error) {
+	if f.chain != nil {
+		return nil, errors.New("filter already chained")
+	}
+
+	f.chain = other
+	return f, nil
+}
---
+++ internal/filter/type_filter_test.go
@@ -0,0 +1,240 @@
+package filter
+
+import (
+	"fmt"
+	"reflect"
+	"regexp"
+	"testing"
+)
+
+func TestNewTypeFilter(t *testing.T) {
+	type args struct {
+		type1        string
+		type2        string
+		excludeTypes []string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    *TypeFilter
+		wantErr bool
+	}{
+		{
+			name:    "should return error if type1 and type2 both empty",
+			wantErr: true,
+		},
+		{
+			name:    "should return error if type2 and excludeTypes both specified",
+			args:    args{type1: "normal", type2: "fairy", excludeTypes: []string{"electric"}},
+			wantErr: true,
+		},
+		{
+			name:    "should return error if one of excluded types is type1 or type2",
+			args:    args{type1: "normal", type2: "", excludeTypes: []string{"normal"}},
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := NewTypeFilter(tt.args.type1, tt.args.type2, tt.args.excludeTypes)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("NewTypeFilter() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("NewTypeFilter() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestTypeFilter_BuildQuery(t *testing.T) {
+	terminalFilterQuery, _ := NewTerminalFilter().BuildQuery()
+
+	type fields struct {
+		type1        string
+		type2        string
+		excludeTypes []string
+		chain        Filter
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		want    string
+		wantErr bool
+	}{
+		{
+			name: "should build correct query if type1 and type2 specified",
+			fields: fields{
+				type1: "normal",
+				type2: "flying",
+				chain: NewMockFilter(),
+			},
+			want: fmt.Sprintf(`
+					SELECT 
+						p.id as pokemon_id 
+					FROM 
+					    pokemon p 
+						LEFT JOIN types t1 on p.type_1_id = t1.id 
+						LEFT JOIN types t2 on p.type_2_id = t2.id 
+					WHERE 
+						(t1.name = 'normal' AND t2.name = 'flying') OR 
+						(t2.name = 'normal' AND t1.name = 'flying') AND  
+						p.id IN (%s)`, mockFilterQuery),
+			wantErr: false,
+		},
+		{
+			name: "should build correct query if only type1 is specified",
+			fields: fields{
+				type1: "normal",
+				chain: NewMockFilter(),
+			},
+			want: fmt.Sprintf(`
+					SELECT 
+						p.id as pokemon_id 
+					FROM 
+					    pokemon p 
+						LEFT JOIN types t1 on p.type_1_id = t1.id 
+						LEFT JOIN types t2 on p.type_2_id = t2.id 
+					WHERE 
+						t1.name = 'normal' OR t2.name = 'normal' AND 
+						p.id IN (%s)`, mockFilterQuery),
+			wantErr: false,
+		},
+		{
+			name: "should build correct query if type1 and excludeTypes specified",
+			fields: fields{
+				type1:        "fire",
+				excludeTypes: []string{"fighting", "psychic", "flying", "dark"},
+				chain:        NewMockFilter(),
+			},
+			want: fmt.Sprintf(`
+					SELECT 
+						p.id as pokemon_id 
+					FROM 
+					    pokemon p 
+						LEFT JOIN types t1 on p.type_1_id = t1.id 
+						LEFT JOIN types t2 on p.type_2_id = t2.id 
+					WHERE 
+						(t1.name = 'fire' AND t2.name NOT IN ('fighting', 'psychic', 'flying', 'dark')) OR 
+						(t2.name = 'fire' AND t1.name NOT IN ('fighting', 'psychic', 'flying', 'dark')) AND 
+						p.id IN (%s)`, mockFilterQuery),
+			wantErr: false,
+		},
+		{
+			name: "should build correct query and chain it with terminal filter when filter is not chained yet",
+			fields: fields{
+				type1: "normal",
+				type2: "flying",
+			},
+			want: fmt.Sprintf(`
+					SELECT 
+						p.id as pokemon_id 
+					FROM 
+					    pokemon p 
+						LEFT JOIN types t1 on p.type_1_id = t1.id 
+						LEFT JOIN types t2 on p.type_2_id = t2.id 
+					WHERE 
+						(t1.name = 'normal' AND t2.name = 'flying') OR 
+						(t2.name = 'normal' AND t1.name = 'flying') AND  
+						p.id IN (%s)`, terminalFilterQuery),
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			f := &TypeFilter{
+				type1:        tt.fields.type1,
+				type2:        tt.fields.type2,
+				excludeTypes: tt.fields.excludeTypes,
+				chain:        tt.fields.chain,
+			}
+			got, err := f.BuildQuery()
+			if (err != nil) != tt.wantErr {
+				t.Errorf("BuildQuery() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+
+			newLineTabRegex, _ := regexp.Compile("[\n\t]")
+			got = newLineTabRegex.ReplaceAllString(got, "")
+			tt.want = newLineTabRegex.ReplaceAllString(tt.want, "")
+
+			spaceRegex, _ := regexp.Compile("\\s+")
+			got = spaceRegex.ReplaceAllString(got, " ")
+			tt.want = spaceRegex.ReplaceAllString(tt.want, " ")
+
+			if got != tt.want {
+				t.Errorf("BuildQuery() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestTypeFilter_Chain(t *testing.T) {
+	terminalFilter := NewTerminalFilter()
+
+	type fields struct {
+		type1        string
+		type2        string
+		excludeTypes []string
+		chain        Filter
+	}
+	type args struct {
+		other Filter
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		want    Filter
+		wantErr bool
+	}{
+		{
+			name: "should add chain to filter if filter not chained yet",
+			fields: fields{
+				type1: "normal",
+				type2: "fairy",
+			},
+			args: args{
+				other: terminalFilter,
+			},
+			want: &TypeFilter{
+				type1: "normal",
+				type2: "fairy",
+				chain: terminalFilter,
+			},
+			wantErr: false,
+		},
+		{
+			name: "should return error if filter already chained",
+			fields: fields{
+				type1: "normal",
+				type2: "fairy",
+				chain: terminalFilter,
+			},
+			args: args{
+				other: terminalFilter,
+			},
+			want:    nil,
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			f := &TypeFilter{
+				type1:        tt.fields.type1,
+				type2:        tt.fields.type2,
+				excludeTypes: tt.fields.excludeTypes,
+				chain:        tt.fields.chain,
+			}
+			got, err := f.Chain(tt.args.other)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Chain() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Chain() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
