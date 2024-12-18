a875433c75277fab5abb27d7c8a2ee5db52c7cf6
Fajri Kornel <fajri.apk@gmail.com>
2024-12-18 11:35:09 +0700

    feat: add AbilityFilter implementation

---
+++ internal/filter/ability_filter.go
@@ -0,0 +1,58 @@
+package filter
+
+import (
+	"errors"
+	"fmt"
+)
+
+type AbilityFilter struct {
+	ability string
+	chain   Filter
+}
+
+func NewAbilityFilter(ability string) (*AbilityFilter, error) {
+	if ability == "" {
+		return nil, errors.New("empty ability")
+	}
+
+	return &AbilityFilter{
+		ability: ability,
+	}, nil
+}
+
+func (f *AbilityFilter) BuildQuery() (string, error) {
+	query := fmt.Sprintf(`
+		SELECT 
+		    p.id as pokemon_id 
+		FROM 
+			pokemon p 
+			LEFT JOIN pokemon_abilities pa on p.id = pa.pokemon_id 
+			LEFT JOIN abilities a on a.id = pa.ability_id 
+		WHERE 
+		    a.name = '%s' 
+	`, f.ability)
+
+	var chainQuery string
+	var err error
+	if f.chain != nil {
+		chainQuery, err = f.chain.BuildQuery()
+	} else {
+		chainQuery, err = NewTerminalFilter().BuildQuery()
+	}
+
+	if err != nil {
+		return "", err
+	}
+
+	query += fmt.Sprintf(" AND p.id IN (%s)", chainQuery)
+	return query, nil
+}
+
+func (f *AbilityFilter) Chain(other Filter) (Filter, error) {
+	if f.chain != nil {
+		return nil, errors.New("filter already chained")
+	}
+
+	f.chain = other
+	return f, nil
+}
---
+++ internal/filter/ability_filter_test.go
@@ -0,0 +1,183 @@
+package filter
+
+import (
+	"fmt"
+	"reflect"
+	"regexp"
+	"testing"
+)
+
+func TestNewAbilityFilter(t *testing.T) {
+	type args struct {
+		ability string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    *AbilityFilter
+		wantErr bool
+	}{
+		{
+			name:    "should return error if ability is empty",
+			wantErr: true,
+		},
+		{
+			name: "should return ability filter if ability is not empty",
+			args: args{ability: "no-guard"},
+			want: &AbilityFilter{
+				ability: "no-guard",
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := NewAbilityFilter(tt.args.ability)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("NewAbilityFilter() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("NewAbilityFilter() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestAbilityFilter_BuildQuery(t *testing.T) {
+	terminalFilterQuery, _ := NewTerminalFilter().BuildQuery()
+
+	type fields struct {
+		ability string
+		chain   Filter
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		want    string
+		wantErr bool
+	}{
+		{
+			name: "should build correct query and chain it with TerminalFilter when filter is not chained yet",
+			fields: fields{
+				ability: "no-guard",
+			},
+			want: fmt.Sprintf(`
+					SELECT 
+						p.id as pokemon_id 
+					FROM 
+					    pokemon p 
+						LEFT JOIN pokemon_abilities pa on p.id = pa.pokemon_id 
+						LEFT JOIN abilities a on a.id = pa.ability_id 
+					WHERE 
+						a.name = 'no-guard' AND 
+						p.id IN (%s)`, terminalFilterQuery),
+			wantErr: false,
+		},
+		{
+			name: "should build correct query and chain it with chained filter when filter is already chained",
+			fields: fields{
+				ability: "no-guard",
+				chain:   NewMockFilter(),
+			},
+			want: fmt.Sprintf(`
+					SELECT 
+						p.id as pokemon_id 
+					FROM 
+					    pokemon p 
+						LEFT JOIN pokemon_abilities pa on p.id = pa.pokemon_id 
+						LEFT JOIN abilities a on a.id = pa.ability_id 
+					WHERE 
+						a.name = 'no-guard' AND 
+						p.id IN (%s)`, mockFilterQuery),
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			f := &AbilityFilter{
+				ability: tt.fields.ability,
+				chain:   tt.fields.chain,
+			}
+			got, err := f.BuildQuery()
+			if (err != nil) != tt.wantErr {
+				t.Errorf("BuildQuery() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+
+			newLineTabRegex, _ := regexp.Compile("[\n\t]")
+			got = newLineTabRegex.ReplaceAllString(got, "")
+			tt.want = newLineTabRegex.ReplaceAllString(tt.want, "")
+
+			spaceRegex, _ := regexp.Compile("\\s+")
+			got = spaceRegex.ReplaceAllString(got, " ")
+			tt.want = spaceRegex.ReplaceAllString(tt.want, " ")
+
+			if got != tt.want {
+				t.Errorf("BuildQuery() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestAbilityFilter_Chain(t *testing.T) {
+	terminalFilter := NewTerminalFilter()
+
+	type fields struct {
+		ability string
+		chain   Filter
+	}
+	type args struct {
+		other Filter
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		want    Filter
+		wantErr bool
+	}{
+		{
+			name: "should add chain to filter if filter not chained yet",
+			fields: fields{
+				ability: "no-guard",
+			},
+			args: args{
+				other: terminalFilter,
+			},
+			want: &AbilityFilter{
+				ability: "no-guard",
+				chain:   terminalFilter,
+			},
+			wantErr: false,
+		},
+		{
+			name: "should return error if filter already chained",
+			fields: fields{
+				ability: "no-guard",
+				chain:   terminalFilter,
+			},
+			args: args{
+				other: terminalFilter,
+			},
+			want:    nil,
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			f := &AbilityFilter{
+				ability: tt.fields.ability,
+				chain:   tt.fields.chain,
+			}
+			got, err := f.Chain(tt.args.other)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Chain() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Chain() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
