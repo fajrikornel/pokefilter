ad0a851e5625c03ce14d98b668ef33788421a818
Fajri Kornel <fajri.apk@gmail.com>
2024-12-18 09:43:33 +0700

    feat: add Filter interface and TerminalFilter implementation

---
+++ internal/filter/filter.go
@@ -0,0 +1,29 @@
+package filter
+
+import "errors"
+
+// Filter interface
+// BuildQuery should return a query string that returns a single column: `pokemon_id`
+// Chain should accept another Filter and incorporate that other Filter's query inside its own.
+type Filter interface {
+	BuildQuery() (string, error)
+	Chain(other Filter) (Filter, error)
+}
+
+type TerminalFilter struct {
+	query string
+}
+
+func NewTerminalFilter() *TerminalFilter {
+	return &TerminalFilter{
+		query: `SELECT id AS pokemon_id FROM pokemon`,
+	}
+}
+
+func (f *TerminalFilter) BuildQuery() (string, error) {
+	return f.query, nil
+}
+
+func (f *TerminalFilter) Chain(_ Filter) (Filter, error) {
+	return nil, errors.New("terminal filter should be the last filter, it cannot be chained with other filters")
+}
